0546b7918862ff9f8fb7b7de5f13ff86a30b9b5c
Дмитрий Сурков <39697992+AdeptSoftware@users.noreply.github.com>
2020-11-21 15:02:05 +0400

    one change

--- git_upload.cmd
+++
@@ -1,9 +0,0 @@
-git init
-git add README.md
-git add requirements.txt
-git add runtime.txt
-git add .
-git commit -m "file"
-git remote add origin https://github.com/AdeptSoftware/SeaBattleGame.git
-git push -u origin master
-pause
